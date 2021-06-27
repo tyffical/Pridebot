d562a5d381770d6fe26435f13848fbd34cf375a1
Tiffany Trinh <34641130+tyffical@users.noreply.github.com>
2021-06-27 15:34:24 +0000

    fix: yolo for ash

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ load_dotenv()
 # emoji info https://gist.github.com/scragly/b8d20aece2d058c8c601b44a689a47a0
 # discord.py docs https://discordpy.readthedocs.io/en/latest/api.html
 # emoji codes https://emojiterra.com/
+# emoji codes https://emojigraph.org/
 # keeping bot alive on repl.it https://www.codementor.io/@garethdwyer/building-a-discord-bot-with-python-and-repl-it-miblcwejz
 # change bot status https://python.plainenglish.io/how-to-change-discord-bot-status-with-discord-py-39219c8fceea
 # "run on repl.it" button https://replit.com/talk/learn/Configuring-GitHub-repos-to-run-on-Replit-and-contributing-back/23948
@@ -45,7 +46,7 @@ blahajgang_guild_id = 825807863146479657
 
 #default -> unicode (see https://emojiterra.com for codes)
 default_list = ["rainbow_flag", "rainbow", "rocket", "sparkles", "night_with_stars", "angry", "sunrise", "pirate_flag", "england", "motorboat", "isle_of_man", "tada", "regional_indicator_p", "regional_indicator_a", "regional_indicator_r", "regional_indicator_t", "regional_indicator_y", "white_check_mark", "x", "smiling_face_with_hearts", "watermelon"]
-default_map = {"rainbow_flag": "\U0001f3f3\uFE0F\u200D\U0001f308", "rainbow": "\U0001f308", "rocket": "\U0001f680", "sparkles": "\u2728", "night_with_stars": "\U0001f303", "angry": "\U0001f620", "sunrise": "\U0001f305", "pirate_flag": "\U0001f3f4\u200D\u2620\uFE0F", "england": "\U0001f3f4\U000e0067\U000e0062\U000e0065\U000e006e\U000e0067\U000e007f", "motorboat": "\U0001f6e5\uFE0F", "isle_of_man": "\U0001f1ee\U0001f1f2", "tada": "\U0001F389", "regional_indicator_p" : "\U0001F1F5", "regional_indicator_a": "\U0001F1E6", "regional_indicator_r": "\U0001F1F7", "regional_indicator_t" : "\U0001F1F9", "regional_indicator_y" : "\U0001F1FE", "white_check_mark" : "\u2705", "x": "\u274C", "smiling_face_with_hearts" : "\U0001f970", "watermelon" : "\U0001F349"}
+default_map = {"rainbow_flag": "\U0001f3f3\uFE0F\u200D\U0001f308", "rainbow": "\U0001f308", "rocket": "\U0001f680", "sparkles": "\u2728", "night_with_stars": "\U0001f303", "angry": "\U0001f620", "sunrise": "\U0001f305", "pirate_flag": "\U0001f3f4\u200D\u2620\uFE0F", "england": "\U0001f3f4\U000e0067\U000e0062\U000e0065\U000e006e\U000e0067\U000e007f", "motorboat": "\U0001f6e5\uFE0F", "isle_of_man": "\U0001f1ee\U0001f1f2", "tada": "\U0001F389", "regional_indicator_p" : "\U0001F1F5", "regional_indicator_a": "\U0001F1E6", "regional_indicator_r": "\U0001F1F7", "regional_indicator_t" : "\U0001F1F9", "regional_indicator_y" : "\U0001F1FE", "white_check_mark" : "\u2705", "x": "\u274C", "smiling_face_with_hearts" : "\U0001f970", "watermelon" : "\U0001F349", "regional_indicator_o": "\U0001f1f4", "regional_indicator_l": "\U0001f1f1", "o2": "\U0001f17e\uFE0F"}
 #TODO: find a way to automate getting the unicodes (web scraping?)
 
 #custom -> discord.Emoji objects
