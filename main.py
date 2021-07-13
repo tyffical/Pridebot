96a7bdc2c7c0571697a247a94e7b8c57998beae2
Tiffany Trinh <34641130+tyffical@users.noreply.github.com>
2021-07-13 13:53:04 +0000

    fix: disabled sentiment analysis until new api found

--- main.py
+++ main.py
@@ -141,25 +141,27 @@ async def on_message(message):
     #ignore bot's own message
     if message.author.id == client.user.id:
         return
-    sentiment = os.environ['sentiment']
-    #sentiment analysis
-    r = requests.post(
-        "https://api.deepai.org/api/sentiment-analysis",
-        data={
-            'text': message.content,
-        },
-        headers={'api-key': sentiment})
-    print(str(message.content))
-    print(r.json()) #prints out id and output: formated as array of ['verypositive/positive/neutral/negative/verynegative']
-    if message.channel.id == sentiment_channel_id:
-        sentiment = str(r.json()["output"][0]).lower()
-        #switch to match-case (aka switch) statements for python 3.10
-        if sentiment == "verypositive" or sentiment == "positive":
-            await message.add_reaction(custom_map["blaheart"])
-        elif sentiment == "neutral":
-            await message.add_reaction(custom_map["melonBLAHAJ"])
-        else:
-            await message.add_reaction(custom_map["yaay"])
+
+    #sentiment analysis disabled until a free api is found
+    # sentiment = os.environ['sentiment']
+    # #sentiment analysis
+    # r = requests.post(
+    #     "https://api.deepai.org/api/sentiment-analysis",
+    #     data={
+    #         'text': message.content,
+    #     },
+    #     headers={'api-key': sentiment})
+    # print(str(message.content))
+    # print(r.json()) #prints out id and output: formated as array of ['verypositive/positive/neutral/negative/verynegative']
+    # if message.channel.id == sentiment_channel_id:
+    #     sentiment = str(r.json()["output"][0]).lower()
+    #     #switch to match-case (aka switch) statements for python 3.10
+    #     if sentiment == "verypositive" or sentiment == "positive":
+    #         await message.add_reaction(custom_map["blaheart"])
+    #     elif sentiment == "neutral":
+    #         await message.add_reaction(custom_map["melonBLAHAJ"])
+    #     else:
+    #         await message.add_reaction(custom_map["yaay"])
 
 
     #react with melon because melon is amazing -adam
