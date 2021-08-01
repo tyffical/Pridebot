bdc27a9e305693467ea0e2cdb981b7ec9921a450
Anirudh  Anup <anirudh012009@gmail.com>
2021-08-01 18:24:23 +0530

    expressionless unicode added
    will be using it for a reactions

--- data/emojis.py
+++ data/emojis.py
@@ -44,7 +44,8 @@ default_map = {
     "sleeping": "\U0001f634",
     "shushing_face": "\U0001f92b",
     "exploding_head": "\U0001f92f",
-    "pizza": "\U0001f355"
+    "pizza": "\U0001f355",
+    "expressionless": "\U0001F611"
 }
 #TODO: find a way to automate getting the unicodes (web scraping?)
 
